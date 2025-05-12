afdab56c5ab886f36af3a5a342021da32753e859
mattiasfe <matferge@gmail.com>
2025-05-12 16:35:05 +0200

    gg

---
+++ Loops/ClassOppgave3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class ClassOppgave3
+    {
+        public void Main()
+        {
+            bool shouldContinue = true;
+            int i = 0;
+            while (shouldContinue == true)
+            {
+                if (i > 9)
+                {
+                    shouldContinue = false;
+                }
+                Console.WriteLine("Runde nr: "+ i);
+                i++;
+            }
+        }
+    }
+}
--- Loops/Program.cs
+++ Loops/Program.cs
@@ -5,3 +5,6 @@ ClassOppgave1.Main();
 
 var ClassOppgave2 = new ClassOppgave2();
 ClassOppgave2.Main();
+
+var ClassOppgave3 = new ClassOppgave3();
+ClassOppgave3.Main();
